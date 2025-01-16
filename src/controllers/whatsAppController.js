21e825984f90f52c506e3a8bfee83a604633d515
Nadav Dori <dori.nadav@gmail.com>
2025-01-16 13:09:50 +0200

    Stop tracking node_modules and .env

--- src/controllers/whatsAppController.js
+++ src/controllers/whatsAppController.js
@@ -1,4 +1,18 @@
 const verifyToken =(req,res) => {
+  try {
+    let accessToken = process.env.FB_API_KEY||''
+    let token = req.query('hub.verify_token')
+    let challenge = req.body('hub.challenge')
+    if(challenge != null && token != null && token === accessToken){
+        res.send(challenge)
+    } else {
+        res.status(400).send()
+    }
+
+  } catch (error) {
+    console.log('had a problem to verify token',error)
+    res.status(400).send
+}
     res.send('hello from verify token')
 }
 const receivedMessages =(req,res) => {
